d06cab664dcc191fc3f382e275cb37c241e9d255
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-08-16 11:32:49 -0700

    adding basic javascript and modified questions html

---
+++ Project2/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#submit").click(function() {
+    var first = $("#que1").val();
+    var second = $("#que2").val();
+    var third = $("#que3").val();
+    var four = $("#que4").val();
+    var five = $("#que5").val();
+    if (first === "ye" && second === "hi" && third === "es" && four === "Fd" && five === "NW") {
+      $("#L1").show();
+      $("#L3").show();
+    } else if (first === "no" )
+
+
+
+
+  });
+});
