e3626301a27d18241f1e014241883d1cd1a17db0
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-08-16 14:02:47 -0700

    trying to fix JQuery function and making combination

--- Project2/js/scripts.js
+++ Project2/js/scripts.js
@@ -5,19 +5,27 @@ $(document).ready(function() {
     var third = $("#que3").val();
     var four = $("#que4").val();
     var five = $("#que5").val();
-    if (first === "ye" && second === "hi" || second === "mo"  && third === "es" && four === "Fd" && five === "NW") {
-      $("#L1").show();
-      $("#L3").show();
-    } else if (first === "no" && second === "mo" || second === "lo"  && third === "on" && four === "hd" && five === "OD") {
+    $(".card").hide();
+    if (first==="ye" &&  second==="hi" && third==="es" && four==="Fd" && five==="NW") {
       $("#L2").show();
       $("#L4").show();
-    } else if (first === "no" && second === "lo" || second === "na" && third === "es" && four === "Fd" && five === "NW") {
-      $("#L5").show();
+    } else if (first==="no" && second==="lo" && third==="on" && four==="hd" && five==="OD" {
       $("#L6").show();
-    } else {
-      alert('please select one of the options above. Thank You!')
+      $("#L4").show();
+    } else if (first === "no" {
+      $("#L1").show();
+      $("#L3").show();
+    } else if (first === "no"  {
+      $("#L5").show();
+      $("#L3").show();
+    } else  {
+      alert ('choose Somthing')
     }
-
-
   });
+
+
+  $("#submit").click(function() {
+    $("#text").show();
+  });
+
 });
