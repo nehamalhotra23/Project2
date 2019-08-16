bb5e2af8afc738376c0e1b0d22d60eb131b50810
Neha Malhotra <nehamalhotra8899@gmail.com>
2019-08-16 12:12:44 -0700

    trying to fix bugs in javascript

--- Project2/js/scripts.js
+++ Project2/js/scripts.js
@@ -5,12 +5,18 @@ $(document).ready(function() {
     var third = $("#que3").val();
     var four = $("#que4").val();
     var five = $("#que5").val();
-    if (first === "ye" && second === "hi" && third === "es" && four === "Fd" && five === "NW") {
+    if (first === "ye" && second === "hi" || second === "mo"  && third === "es" && four === "Fd" && five === "NW") {
       $("#L1").show();
       $("#L3").show();
-    } else if (first === "no" )
-
-
+    } else if (first === "no" && second === "mo" || second === "lo"  && third === "on" && four === "hd" && five === "OD") {
+      $("#L2").show();
+      $("#L4").show();
+    } else if (first === "no" && second === "lo" || second === "na" && third === "es" && four === "Fd" && five === "NW") {
+      $("#L5").show();
+      $("#L6").show();
+    } else {
+      alert('please select one of the options above. Thank You!')
+    }
 
 
   });
